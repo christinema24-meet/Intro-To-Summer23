c3bbaf0da276b55a3f8c9e812f0d14e551f2b067
christinema24-meet <christinemattar88@gmail.com>
2024-08-21 19:30:18 +0300

    python lab

--- christine.py
+++ christine.py
@@ -1,2 +1,18 @@
-print("hello world")
-print("christine")
+def create_youtube_video (title,description):
+	video = {"title": title,"description": description,"likes":0,"dislikes":0, "comments":{}}
+	return video
+
+def like (youtube_video):
+	if "likes" in video:
+		video["likes"] +=1
+	return youtube_video
+
+def dislike (youtube_video):
+	if "dislikes" in video:
+		video["dislikes"] +=1
+	return youtube_video
+
+def add_comment(youtubevideo, username, comment_text):
+	
+
+
